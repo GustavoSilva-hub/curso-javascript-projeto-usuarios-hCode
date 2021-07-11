13ae3f906d47a159e0cf06c1c6a0f0e0ccef264e
Gustavo <gustavo.ferreira0821@gmail.com>
2021-07-11 09:51:47 -0300

    The users count is done

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,24 +15,28 @@ class UserController {
 
             console.log(this.formEl.reset);
 
-            btn.disabled=true;
+            btn.disabled = true;
 
             let values = this.getFormValues();
 
-            this.getPhoto().then(
-                 (content) => {
-                    values.photo = content;
-                    this.addLine(values);
-                    this.formEl.reset();
-                    btn.disabled = false;
-                }, (error) => {
-                    console.error(error);
-                }
-            )
+            if (values) {
+                this.getPhoto().then(
+                    (content) => {
+                        values.photo = content;
+                        this.addLine(values);
+                        this.formEl.reset();
+                        btn.disabled = false;
+                    }, (error) => {
+                        console.error(error);
+                    }
+                )
+            }
+
+
         })
 
     }
- 
+
 
     getPhoto() {
         return new Promise((resolve, reject) => {
@@ -54,9 +58,9 @@ class UserController {
                 reject(e);
             }
 
-            if(filePhoto){
+            if (filePhoto) {
                 fileReader.readAsDataURL(filePhoto);
-            }else{
+            } else {
                 resolve('dist/img/boxed-bg.jpg');
             }
         })
@@ -65,21 +69,34 @@ class UserController {
     getFormValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+
+                isValid = false;
+            }
+
+
             if (field.name == "gender") {
 
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
 
-            } else if(field.name=="admin"){
+            } else if (field.name == "admin") {
                 user[field.name] = field.checked;
-            }else{
-                user[field.name]=field.value;
+            } else {
+                user[field.name] = field.value;
             }
 
         })
+
+        if (!isValid) {
+            return isValid;
+        }
 
         return new User(
             user.name,
@@ -96,13 +113,15 @@ class UserController {
 
         let userLine = document.createElement("tr");
 
+        userLine.dataset.user = JSON.stringify(dataUser);
+
         userLine.innerHTML = `
         <td>
           <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
         </td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin?'Sim':'Não'}</td>
+        <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
         <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -110,6 +129,25 @@ class UserController {
         </td>`
 
         this.tBodyEl.appendChild(userLine);
+
+        this.updateCount();
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tBodyEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+        });
+        document.getElementById('numbers-of-users').innerText=`${numberUsers}`;
+        document.getElementById('numbers-of-admins').innerText=`${numberAdmin}`;
     }
 
 
